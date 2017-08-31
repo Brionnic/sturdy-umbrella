611ccd465cd1281e6a638de08335c7113e780db5
Brian Hardenstein <bhardens@yahoo.com>
2017-08-30 17:11:22 -0700

    start building data process pipeline

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -2,4 +2,18 @@ import pandas as pd
 import numpy as np
 
 class GetData():
-    def __init__(self, file="../data/"):
+    def __init__(self):
+        # don't really do anything here
+        # but should have
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+
+    def transform(self):
+        # transform to log prices
+        self.y = np.log(y)
+
+    def fit_transform(self, X, y):
+        self.fit(X, y)
+        self.transform()
